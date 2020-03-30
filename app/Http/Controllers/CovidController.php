234182f2d98e965fdcbbe9cb8c505a5a5a9a65df
aditiakusuma <aditiakusuma800@gmail.com>
2020-03-30 22:29:35 +0700

    membuat chart dengan mengambil data API

---
+++ app/Http/Controllers/CovidController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+//butuh use HTTP
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Arr;
+use App\Charts\IndonesiaProvinsiChart;
+
+class CovidController extends Controller
+{
+    public function provinsi()
+    {
+        //fungsi flatten untuk menghilangkan atribut berdasarkan level
+        $suspects       = Http::get('https://api.kawalcorona.com/indonesia/provinsi')->json();
+        //$suspectData    = $suspects->flatten(1)->value()->All();
+        //$suspectData = Arr::flatten($suspects,1);
+        //dd($suspects);
+        //melakukan pluk yaitu ambil data dengan label tertentu misal saja propinsi lihat json nya
+        //pluk terbarubbisa mengambil langsung atribut di bawahnya satu level
+        $provinsi   =Arr::pluck($suspects,'attributes.Provinsi');
+        $positif    =Arr::pluck($suspects,'attributes.Kasus_Posi');
+        $sembuh     =Arr::pluck($suspects,'attributes.Kasus_Semb');
+        $meninggal  =Arr::pluck($suspects,'attributes.Kasus_Meni');
+        //membuat chart
+        $chart  = new IndonesiaProvinsiChart;
+        $chart  ->labels($provinsi);
+        $chart  ->dataset('Data Positif Corona di Indonesia', 'bar', $positif)
+                ->options([
+                    'color' => '#000000',
+                    'backgroundColor' => '#000000',
+                ]);
+        // dd($chart);
+        return view('indonesia',compact('chart'));
+    }
+}
